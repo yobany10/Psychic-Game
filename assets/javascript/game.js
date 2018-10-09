e028630d8500ac50791f9d5a3551b2aea60207cd
Yobany <yobanydesign@gmail.com>
2018-10-08 23:00:00 -0400

    made major updates to html and javascript files but still fixing a few bugs within these two files

---
+++ assets/javascript/game.js
@@ -0,0 +1,58 @@
+
+
+
+//array for possible computer choices//
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//variables to track user information// 
+var userWins = 0;
+var userLosses = 0;
+var guessesRemaining = 9;
+var userTries = [];
+var computerGuess = [];
+
+//intial computer letter//
+window.onload = function() {
+    var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerGuess.push(compGuess);
+    console.log(computerGuess[0]);
+}
+
+//game//
+document.onkeyup = function(event) {
+    var playerGuess = event.key;
+    userTries.push(playerGuess);
+    console.log(computerGuess[0]);
+
+    if ((playerGuess === computerGuess[0]) && (guessesRemaining > 0)) {
+        userWins++;
+        guessesRemaining = 9;
+        userTries.length = 0;
+        var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        computerGuess.push(compGuess);
+        console.log(computerGuess[0]);
+    }
+
+    else if ((playerGuess !== computerGuess[0]) && (guessesRemaining > 0)) {
+        guessesRemaining = guessesRemaining-1;
+    }
+
+    else {
+        userLosses++;
+        guessesRemaining = 9;
+        userTries.length = 0;
+        computerGuess.length = 0;
+        var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        computerGuess.push(compGuess);
+        console.log(computerGuess[0]);
+    }
+
+    var html = "<p>Guess what letter I'm thinking of</p>" +
+                "<p>Wins: " + userWins + "</p>" +
+                "<p>Losses: " + userLosses + "</p>" +
+                "<p>Guesses left: " + guessesRemaining + "</p>" +
+                "<p>Your guesses so far: " + userTries + "</p>";
+
+    document.querySelector("#game").innerHTML = html;
+
+}
